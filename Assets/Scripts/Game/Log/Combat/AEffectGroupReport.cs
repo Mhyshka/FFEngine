b1c50eba17343773b8cf04d72770c594300b4f8a
Mhyshka <elunywow@gmail.com>
2015-07-26 04:02:14 +0200

    Working Effect Over Time Apply / Refresh / Tick / TimeOut lifecycle.

---
+++ Assets/Scripts/Game/Log/Combat/AEffectGroupReport.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class AEffectGroupReport : AEffectReport
+{
+	#region Properties
+	internal List<AEffectReport> effects = new List<AEffectReport>();
+	
+	protected HashSet<EDamageType> _damageTypes;
+	internal HashSet<EDamageType> DamageTypes
+	{
+		get
+		{
+			return _damageTypes;
+		}
+	}
+	
+	internal override bool IsKillingBlow
+	{
+		get
+		{
+			foreach(AEffectReport each in effects)
+			{
+				if(each.IsKillingBlow)
+					return true;
+			}
+			return false;
+		}
+	}
+	
+	internal override int FinalDamages
+	{
+		get
+		{
+			int total = 0;
+			foreach(AEffectReport each in effects)
+			{
+				total += each.FinalDamages;
+			}
+			return total;
+		}
+	}
+	
+	internal override int UnreducedDamages
+	{
+		get
+		{
+			int total = 0;
+			foreach(AEffectReport each in effects)
+			{
+				total += each.UnreducedDamages;
+			}
+			return total;
+		}
+	}
+	
+	internal override int ReducedByArmor
+	{
+		get
+		{
+			int total = 0;
+			foreach(AEffectReport each in effects)
+			{
+				total += each.ReducedByArmor;
+			}
+			return total;
+		}
+	}
+	#endregion
+}
--- Assets/Scripts/Game/Log/Combat/EffectOverTimeReport.cs
+++ Assets/Scripts/Game/Log/Combat/EffectOverTimeReport.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public abstract class EffectOverTimeReport : AEffectReport
+public class EffectOverTimeReport : EffectOverTimeReport
 {
-	internal EffectOverTime effect = null;
+	internal EEffectApplyResult applyResult = EEffectApplyResult.NotApplied;
+	internal List<AEffectReport> appliedEffects = null;
+
+	internal EffectOverTimeConf effect = null;
 	
 	internal override EReportAlignement Alignement
 	{
@@ -12,18 +16,79 @@ public abstract class EffectOverTimeReport : AEffectReport
 			return EReportAlignement.Neutral;
 		}
 	}
-	
+
 	internal override EReportLevel Level
 	{
 		get
 		{
-			return EReportLevel.Normal;
+			switch(applyResult)
+			{
+				case EEffectApplyResult.Applied : 
+				return EReportLevel.Reduced;
+				
+				case EEffectApplyResult.Refreshed :
+				return EReportLevel.Reduced;
+				
+				case EEffectApplyResult.NotApplied :
+				return EReportLevel.Verbose;
+				
+				case EEffectApplyResult.Resisted :
+				return EReportLevel.Normal;
+				
+				case EEffectApplyResult.Immune :
+				return EReportLevel.Normal;
+			}
+			
+			return EReportLevel.Verbose;
 		}
 	}
-	/*
+	
 	public override string ToString ()
 	{
-		//TODO LOCALIZATION
-		return string.Format ("[EffectOverTimeReport: ]");
-	}*/
+		string report = "";
+		for(int i = 0 ; i < indentLevel ; i++)
+			report += "\t";
+		if(applyResult == EEffectApplyResult.Applied)
+		{
+			report += string.Format("{0} is affected by {1}'s {2}.", target.Name,
+																	attackInfos.source.Name,
+																	effect.Name);
+			foreach(AEffectReport each in appliedEffects)
+			{
+				each.indentLevel = indentLevel + 1;
+				report += "\n" + each.ToString();
+			}
+		}
+		else if(applyResult == EEffectApplyResult.Refreshed)
+		{
+			report += string.Format("{0}'s {1} is refreshed on {2}.", attackInfos.source.Name,
+			                       									effect.Name,
+												                    target.Name);
+			foreach(AEffectReport each in appliedEffects)
+			{
+				each.indentLevel = indentLevel + 1;
+				report += "\n" + each.ToString();
+			}
+		}
+		else if(applyResult == EEffectApplyResult.NotApplied)
+		{
+			report += string.Format("{0}'s {1} couldn't be applied on {2}.", attackInfos.source.Name,
+													                        effect.Name,
+													                        target.Name);
+		}
+		else if(applyResult == EEffectApplyResult.Resisted)
+		{
+			report += string.Format("{0} resisted to {1}'s {2}.", target.Name,
+										                        attackInfos.source.Name,
+										                        effect.Name);
+		}
+		else if(applyResult == EEffectApplyResult.Immune)
+		{
+			report += string.Format("{0} was immune to {1}'s {2}.", target.Name,
+											                        attackInfos.source.Name,
+											                        effect.Name);
+		}
+		
+		return report;
+	}
 }
