f808b547877ff5288f55d9f303a2f11a3ad762b9
RaphaelPinto <raphael.pinto38@gmail.com>
2015-09-12 01:25:04 +0200

    fuck this

---
+++ Assets/Addons/ffbackgroundtask/iOSBackgroundTask.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+
+public class iOSBackgroundTask 
+{
+	#region Background task
+	[DllImport ("__Internal")]
+	private static extern void startBackgroundTask(string alertTitle, string alertBody, string alertAction, int deltaDealTime);
+	[DllImport ("__Internal")]
+	private static extern float registerForPushNotification();
+
+
+	// Use this for initialization
+	static void ios_registerForPushNotification () 
+	{
+		#if UNITY_IOS && !UNITY_EDITOR
+		registerForPushNotification ();
+		#endif
+	}
+	
+	// Update is called once per frame
+	static void ios_startBackgroundTask (string alertTitle, string alertBody, string alertAction, int deltaDealTime) 
+	{
+		#if UNITY_IOS && !UNITY_EDITOR
+		startBackgroundTask(alertTitle, alertBody, alertAction, deltaDealTime);
+		#endif
+	}
+	#endregion
+}
